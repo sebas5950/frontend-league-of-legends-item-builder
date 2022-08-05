a933af3a35e6136488a0f2d96142df27910a4443
Joon Jung <joonjung55@gmail.com>
2022-08-05 12:00:57 -0400

    one css

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,7 +20,6 @@ function Home({ user, championsData }) {
       <source src={video} type="video/mp4"/>
         </video>
       <div className="logged-in-home">
-      <h1 class="hero_title">LEAGUE OF LEGENDS ITEM BUILDER</h1>
         <h2 class="home_title2">Welcome Back, {user.username}!</h2>
       </div>
       </section>
@@ -43,7 +42,16 @@ function Home({ user, championsData }) {
       <source src={video} type="video/mp4"/>
         </video>
       <div className="signup-home">
-      <h1 class="hero_title">LEAGUE OF LEGENDS ITEM BUILDER</h1>
+      <img src='https://cdn.discordapp.com/attachments/981197460493979738/1005141787838529536/logo-1200-.png'  
+    style={{   position: "absolute",
+    
+    left: "50%",
+    top: "33%",
+   width: "40%",
+      
+    transform: "translate(-50%, -50%)",
+    zIndex: "-1",
+    filter: "blur(0px)"}}/>
         <h2 class="home_title">Please Login or Sign-up</h2>
       </div>
       </section>
